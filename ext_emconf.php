0509550276f0f2b70eb94a5d82f8bdc9deee8000
sensomedia <sensomedia@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-03-15 19:32:11 +0000

    added description
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/metaext/trunk@17824 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "metaext"
 #
-# Auto generated 11-03-2009 19:45
+# Auto generated 15-03-2009 19:52
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -12,7 +12,7 @@
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Config, Metatag & SEO Features',
-	'description' => '',
+	'description' => 'This extension provides you with an editable set of all the important \'config\' parameters including multilanguage settings, adds additional metatag fields to the page module and provides global settings for some of them (those who make sense to be filled globaly). It also comes with a configurable html post processor which is able to remove html comments (if not inside script/style tag) & redundant whitespace, making a nice indentation and even reorder the tags within the html header (eg. pushing title & metatags on top).',
 	'category' => 'be',
 	'author' => 'Michael \'Iggy\' Rudolph - sensomedia.de',
 	'author_email' => 'info@sensomedia.de',
@@ -39,7 +39,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:21:{s:9:".htaccess";s:4:"cad3";s:9:"ChangeLog";s:4:"c829";s:10:"README.txt";s:4:"ee2d";s:21:"ext_conf_template.txt";s:4:"1f84";s:12:"ext_icon.gif";s:4:"76f6";s:17:"ext_localconf.php";s:4:"73f0";s:14:"ext_tables.php";s:4:"7426";s:14:"ext_tables.sql";s:4:"feba";s:16:"locallang_db.xml";s:4:"a428";s:11:"doc/man.sxw";s:4:"d83e";s:14:"doc/manual.ott";s:4:"39db";s:14:"doc/manual.sxw";s:4:"5321";s:73:"doc/typo3.org Documentation Documentation template(Table of Contents).URL";s:4:"584d";s:36:"lib/class.tx_metaext_postprocess.php";s:4:"53b2";s:35:"lib/class.ux_t3lib_tsparser_ext.php";s:4:"2442";s:27:"lib/class.ux_tslib_menu.php";s:4:"e650";s:38:"modfunc1/class.tx_metaext_modfunc1.php";s:4:"932f";s:22:"modfunc1/locallang.xml";s:4:"a40c";s:20:"static/constants.txt";s:4:"6da6";s:13:"static/lib.ts";s:4:"8e2e";s:16:"static/setup.txt";s:4:"7462";}',
+	'_md5_values_when_last_written' => 'a:18:{s:9:".htaccess";s:4:"cad3";s:9:"ChangeLog";s:4:"c829";s:10:"README.txt";s:4:"ee2d";s:21:"ext_conf_template.txt";s:4:"1f84";s:12:"ext_icon.gif";s:4:"76f6";s:17:"ext_localconf.php";s:4:"d170";s:14:"ext_tables.php";s:4:"7426";s:14:"ext_tables.sql";s:4:"feba";s:16:"locallang_db.xml";s:4:"a428";s:14:"doc/manual.sxw";s:4:"d83e";s:36:"lib/class.tx_metaext_postprocess.php";s:4:"07bc";s:35:"lib/class.ux_t3lib_tsparser_ext.php";s:4:"2442";s:27:"lib/class.ux_tslib_menu.php";s:4:"e650";s:38:"modfunc1/class.tx_metaext_modfunc1.php";s:4:"932f";s:22:"modfunc1/locallang.xml";s:4:"a40c";s:20:"static/constants.txt";s:4:"037f";s:13:"static/lib.ts";s:4:"8e2e";s:16:"static/setup.txt";s:4:"613e";}',
 	'suggests' => array(
 	),
 );
