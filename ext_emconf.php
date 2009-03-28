e08bc1a9da111e46677263aaad8e390e0d7596ec
sensomedia <sensomedia@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-03-28 05:47:27 +0000

    version 0.4.0 TER
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/metaext/trunk@18461 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "metaext"
 #
-# Auto generated 27-03-2009 11:23
+# Auto generated 28-03-2009 06:40
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 1,
 	'lockType' => '',
 	'author_company' => 'sensomedia.de',
-	'version' => '0.3.2',
+	'version' => '0.4.0',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '4.2.0-0.0.0',
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 			'realurl' => '1.5.3-0.0.0',
 		),
 	),
-	'_md5_values_when_last_written' => 'a:19:{s:9:".htaccess";s:4:"cad3";s:9:"ChangeLog";s:4:"2157";s:10:"README.txt";s:4:"ee2d";s:21:"ext_conf_template.txt";s:4:"af93";s:12:"ext_icon.gif";s:4:"5a59";s:17:"ext_localconf.php";s:4:"eddf";s:14:"ext_tables.php";s:4:"7426";s:14:"ext_tables.sql";s:4:"feba";s:16:"locallang_db.xml";s:4:"a428";s:14:"doc/manual.sxw";s:4:"17ef";s:36:"lib/class.tx_metaext_postprocess.php";s:4:"fb5c";s:32:"lib/class.tx_metaext_sitemap.php";s:4:"a796";s:35:"lib/class.ux_t3lib_tsparser_ext.php";s:4:"8a6f";s:27:"lib/class.ux_tslib_menu.php";s:4:"e650";s:38:"modfunc1/class.tx_metaext_modfunc1.php";s:4:"932f";s:22:"modfunc1/locallang.xml";s:4:"a40c";s:20:"static/constants.txt";s:4:"e202";s:13:"static/lib.ts";s:4:"8e2e";s:16:"static/setup.txt";s:4:"9c36";}',
+	'_md5_values_when_last_written' => 'a:19:{s:9:".htaccess";s:4:"cad3";s:9:"ChangeLog";s:4:"2157";s:10:"README.txt";s:4:"ee2d";s:21:"ext_conf_template.txt";s:4:"9437";s:12:"ext_icon.gif";s:4:"5a59";s:17:"ext_localconf.php";s:4:"eddf";s:14:"ext_tables.php";s:4:"f175";s:14:"ext_tables.sql";s:4:"27ce";s:16:"locallang_db.xml";s:4:"1e91";s:14:"doc/manual.sxw";s:4:"17ef";s:36:"lib/class.tx_metaext_postprocess.php";s:4:"2b80";s:32:"lib/class.tx_metaext_sitemap.php";s:4:"c60c";s:35:"lib/class.ux_t3lib_tsparser_ext.php";s:4:"8a6f";s:27:"lib/class.ux_tslib_menu.php";s:4:"e650";s:38:"modfunc1/class.tx_metaext_modfunc1.php";s:4:"984e";s:22:"modfunc1/locallang.xml";s:4:"5e1e";s:20:"static/constants.txt";s:4:"6944";s:13:"static/lib.ts";s:4:"8e2e";s:16:"static/setup.txt";s:4:"7c08";}',
 	'suggests' => array(
 	),
 );
